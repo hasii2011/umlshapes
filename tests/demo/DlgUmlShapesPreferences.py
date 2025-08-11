dc59e79be1672eb0e84daac95cba8601bb7927a2
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2025-08-11 09:01:20 -0500

    <Initial>[Preferences UI]: <Start of updated preferences>
    [
    * Better names from OGL
    * Use the demo app to demo the dialog
    ]
    
    [https://github.com/hasii2011/umlshapes/issues/20]

---
+++ tests/demo/DlgUmlShapesPreferences.py
@@ -0,0 +1,77 @@
+
+from logging import Logger
+from logging import getLogger
+
+from wx import DEFAULT_DIALOG_STYLE
+from wx import EVT_BUTTON
+from wx import EVT_CLOSE
+from wx import ID_CANCEL
+from wx import ID_OK
+from wx import NB_FIXEDWIDTH
+from wx import NB_TOP
+from wx import Notebook
+from wx import OK
+from wx import CANCEL
+
+from wx import CommandEvent
+from wx import RESIZE_BORDER
+from wx import Size
+from wx import StdDialogButtonSizer
+
+from wx.lib.sized_controls import SizedDialog
+from wx.lib.sized_controls import SizedPanel
+
+from umlshapes.dialogs.preferences.DefaultValuesPanel import DefaultValuesPanel
+
+
+class DlgUmlShapesPreferences(SizedDialog):
+
+    def __init__(self, parent):
+
+        style:   int  = DEFAULT_DIALOG_STYLE | RESIZE_BORDER
+        dlgSize: Size = Size(460, 440)
+
+        super().__init__(parent, title='Ogl Preferences', size=dlgSize, style=style)
+
+        self.logger: Logger     = getLogger(__name__)
+        sizedPanel:  SizedPanel = self.GetContentsPane()
+        sizedPanel.SetSizerType('vertical')
+        sizedPanel.SetSizerProps(proportion=1, expand=True)
+
+        nbStyle: int = NB_TOP | NB_FIXEDWIDTH
+        book: Notebook = Notebook(sizedPanel, style=nbStyle)
+        book.SetSizerProps(expand=True, proportion=1)
+
+        # diagramPreferencesPage:       DiagramPreferencesPage       = DiagramPreferencesPage(parent=book)
+        defaultValuesPreferencesPage: DefaultValuesPanel = DefaultValuesPanel(parent=book)
+
+        # book.AddPage(diagramPreferencesPage,       text=diagramPreferencesPage.name,       select=True)
+        book.AddPage(defaultValuesPreferencesPage, text=defaultValuesPreferencesPage.name, select=False)
+
+        self._layoutStandardOkCancelButtonSizer()
+        # self.Fit()
+        # self.SetMinSize(self.GetSize())
+
+    def _layoutStandardOkCancelButtonSizer(self):
+        """
+        Call this last when creating controls; Will take care of
+        adding callbacks for the Ok and Cancel buttons
+        """
+        buttSizer: StdDialogButtonSizer = self.CreateStdDialogButtonSizer(OK | CANCEL)
+
+        self.SetButtonSizer(buttSizer)
+        self.Bind(EVT_BUTTON, self._onOk,    id=ID_OK)
+        self.Bind(EVT_BUTTON, self._onClose, id=ID_CANCEL)
+        self.Bind(EVT_CLOSE,  self._onClose)
+
+    # noinspection PyUnusedLocal
+    def _onOk(self, event: CommandEvent):
+        """
+        """
+        self.EndModal(OK)
+
+    # noinspection PyUnusedLocal
+    def _onClose(self, event: CommandEvent):
+        """
+        """
+        self.EndModal(CANCEL)
